2cde3c63c0ff94064627855d9a6dc20ab0e7b9c9
RE-Pablo <pablo.rames.07@gmail.com>
2024-04-27 00:59:07 -0600

    geolocalizacion commit

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+// MENU DE HAMBURGESA
+
 const burgerBtn = document.getElementById('burgerBtn');
 const menu = document.getElementById("menu");
 
@@ -19,3 +21,33 @@ function controlarMenu() {
 
 window.addEventListener('load', controlarMenu);
 window.addEventListener('resize', controlarMenu);
+
+// API GOOGLE MAPS
+function initMap() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: position.coords.latitude, lng: position.coords.longitude}, // Ubicación del usuario
+                zoom: 12
+            });
+            var input = document.getElementById('ubicacion');
+            var autocomplete = new google.maps.places.Autocomplete(input);
+            autocomplete.bindTo('bounds', map);
+        }, function() {
+            handleLocationError(true, map.getCenter());
+        });
+    } else {
+        handleLocationError(false, map.getCenter());
+    }
+}
+function handleLocationError(browserHasGeolocation, pos) {
+    // Manejar errores de geolocalización
+    // Por ejemplo, centrar en una ubicación predeterminada si la geolocalización no está disponible
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 19.4326, lng: -99.1332}, // Centro de la Ciudad de México
+        zoom: 12
+    });
+    var input = document.getElementById('ubicacion');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+}
